c31122dabc1543a50274fa46b15a3bb7835bf5e8
gabriel-vieira2000 <gvc20002016@gmail.com>
2023-01-23 08:02:30 -0300

    Resolvendo exercicio 6 e Desafio

--- src/aula6_VetoresMatrizesMetodos/Exercicio09.java
+++ src/aula6_VetoresMatrizesMetodos/Exercicio09.java
@@ -1,5 +1,7 @@
 package aula6_VetoresMatrizesMetodos;
 
+import java.util.Random;
+
 public class Exercicio09 {
 
 	public static void main(String[] args) {
@@ -7,7 +9,12 @@ public class Exercicio09 {
 		Random numerosAleatorios = new Random();
 		
 		for(int i=0; i<6;i++) {
-			numerosJogoMegaSena = numerosAleatorios.nextInt(0,60);
+			numerosJogoMegaSena[i] = numerosAleatorios.nextInt(0,60);
+		}
+		
+		System.out.print("JOGO DE MEGA SENA GERADO: ");
+		for(int i=0; i<6; i++) {
+			System.out.print(numerosJogoMegaSena[i] + "\t");
 		}
 
 	}
