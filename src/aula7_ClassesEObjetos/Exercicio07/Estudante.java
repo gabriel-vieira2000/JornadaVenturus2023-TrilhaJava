9e69a502d184dca4c43a8299a7bd9df0076d8231
gabriel-vieira2000 <gvc20002016@gmail.com>
2023-01-23 10:06:02 -0300

    Resolvendo Exercicio 7

---
+++ src/aula7_ClassesEObjetos/Exercicio07/Estudante.java
@@ -0,0 +1,21 @@
+package aula7_ClassesEObjetos.Exercicio07;
+
+public class Estudante {
+	String nome;
+	float notaTrimestre1, notaTrimestre2, notaTrimestre3;
+	
+	float notaFinalAno() {
+		return notaTrimestre1 + notaTrimestre2 + notaTrimestre3;
+	}
+	
+	void verificaSituacao() {
+		float minimoAprovacao = 60f;
+	
+		if(notaFinalAno() >= minimoAprovacao) {
+			System.out.println("O ESTUDANTE ESTA APROVADO!");
+		}else {
+			System.out.println("O ESTUDANTE ESTA REPROVADO!");
+			System.out.printf("FALTOU %.1f PONTOS PARA A APROVACAO! %n",(minimoAprovacao-notaFinalAno()));
+		}
+	}
+}
