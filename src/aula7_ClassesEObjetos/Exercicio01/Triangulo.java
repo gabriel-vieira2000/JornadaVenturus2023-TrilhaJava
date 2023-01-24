9cb13615a0203c8fdc8405b8d9c64fae2a56150e
Gabriel Vieira Cardoso <45353735+gabriel-vieira2000@users.noreply.github.com>
2023-01-23 21:59:31 -0300

    Delete src/aula7_ClassesEObjetos directory

--- src/aula7_ClassesEObjetos/Exercicio01/Triangulo.java
+++
@@ -1,9 +0,0 @@
-package aula7_ClassesEObjetos.Exercicio01;
-
-public class Triangulo {
-	float ladoA, ladoB, ladoC;
-	double area() {
-		double p = (ladoA+ladoB+ladoC)/2;
-		return Math.sqrt(p * (p-ladoA) * (p-ladoB) * (p-ladoC));
-	}
-}
